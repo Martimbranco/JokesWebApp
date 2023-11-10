46104915a173f718fc642b79c382890b66c201af
50251 <50251@aebatalha.ccems.pt>
2023-11-10 15:38:09 +0000

    Adicionar arquivos de projeto.

---
+++ JokesWebApp/Data/Migrations/20231110150840_piadas_do_kazzio.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JokesWebApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class piadas_do_kazzio : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Autor",
+                table: "jokes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Autor",
+                table: "jokes");
+        }
+    }
+}
---
+++ JokesWebApp/Models/jokes.cs
@@ -0,0 +1,21 @@
+﻿namespace JokesWebApp.Models
+{
+    public class jokes
+    {
+        public int id  { get; set; }
+        public string Autor { get; set; }
+        public string jokesPergunta  { get; set; }
+        public string JokeResposta { get; set; }
+
+        public jokes()
+        {
+            id = 0;
+            Autor = string.Empty;
+            jokesPergunta = string.Empty;
+            JokeResposta = string.Empty;
+              
+        }
+
+
+    }
+}
